81f68edb8033ce24e1c0e5494abe45fcf4c9ebbb
Junjay Tan <junjay@junjaytan.com>
2017-06-23 00:51:25 -0400

    some ch2 examples

---
+++ go-book/ch2/set_gopath.sh
@@ -0,0 +1,3 @@
+# must run the command below in terminal to set gopath
+# in order for package to be found
+export GOPATH=$PWD/..
---
+++ go-book/ch2/src/tempconv/tempconv.go
@@ -0,0 +1,16 @@
+// performs Celsius and Fahrenheit conversions.
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+  AbsoluteZeroC Celsius = -273.15
+  FreezingC     Celsius = 0
+  BoilingC      Celsius = 100
+)
+
+func (c Celsius) String() string { return fmt.Sprintf("%gdegC", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%gdegF", f) }
